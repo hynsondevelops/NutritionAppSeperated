a51d783e62d33791bac914ea097033ba57036d74
Adam Hynson <hynsondevelops@gmail.com>
2018-03-16 09:38:29 -0400

    Added FoodSelector and FoodRow

---
+++ client/src/components/FoodSelector.js
@@ -0,0 +1,112 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+import axios from 'axios';
+import FoodRow from './FoodRow';
+
+export default class FoodSelector extends React.Component {
+    static propTypes = {
+    	 searchedFoods: PropTypes.array,
+       day: PropTypes.string
+    };
+
+    /**
+    * @param props - Comes from your rails view.
+    */
+    constructor(props) {
+    super(props);
+
+    // How to set initial state in ES6 class syntax
+    // https://facebook.github.io/react/docs/reusable-components.html#es6-classes
+    this.state = {searchedFoods: [], searchString: null};
+    }
+
+    
+
+    
+
+
+    initialAjaxSuccess = (data) => {
+      console.log(data)
+        this.setState({searchedFoods: []})
+        //no result found
+        if (data.list == null){
+            let placeholderFood = {
+              "ndbno": "0",
+              "name": "Sorry, no results for that search.",
+              "nutrients": [
+                {
+                  "nutrient_id": "208",
+                  "name": "Energy",
+                  "derivation": "LCCS",
+                  "group": "Proximates",
+                  "unit": "kcal",
+                  "value": "N/A",
+                  "measures": [
+                    {
+                      "label": "ONZ",
+                      "eqv": 0,
+                      "eunit": "g",
+                      "qty": 0,
+                      "value": "0"
+                    }
+                  ]
+                }
+                ]
+            }
+            this.setState({searchedFoods: [placeholderFood]})
+        }
+        //makes AJAX calls for detailed info on all foods found in results
+        for (let i = 0; i < data.list.item.length; i++){
+            let APIURL = "https://api.nal.usda.gov/ndb/reports/?ndbno=" + data.list.item[i].ndbno + "&type=f&format=json&api_key=hyMAaC37dIT57p36cBZ1Sn6tK5XYfnOLP4IaNSs7"
+            axios(APIURL).then(result => this.detailedAjaxSuccess(result.data))
+        }
+    }
+
+    detailedAjaxSuccess = (data) => {
+      console.log("Detailed")
+        this.setState({searchedFoods: this.state.searchedFoods.concat([data.report.food])})
+    }
+
+
+
+
+    handleChange = (event) => {
+      console.log(event.target.value)
+        this.setState({searchString: event.target.value});
+        //making call to USDA database 
+        let APIURL = ("https://api.nal.usda.gov/ndb/search/?format=json&q=" + event.target.value + "&sort=n&max=25&offset=0&api_key=hyMAaC37dIT57p36cBZ1Sn6tK5XYfnOLP4IaNSs7")
+        axios(APIURL, {
+          responseType: 'json'}).then(result => this.initialAjaxSuccess(result.data))
+    }
+
+    render() {
+      const foodRows = this.state.searchedFoods.map((food) =>
+          <FoodRow key={food["ndbno"]} searchedFood={food} searchOrDaily={true} day={this.props.day} />
+        );
+    return (
+        <div>
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Search:
+          <input className="food_search" type="text" value={this.state.value} onChange={this.handleChange} />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+    	<table className="table">
+        <thead>
+          <tr>
+            <th scope="col">Name</th>
+            <th scope="col">Amount</th>
+            <th scope="col">Serving Size</th>
+            <th scope="col">Calories</th>
+          </tr>
+        </thead>
+        <tbody>
+          {foodRows }
+          
+        </tbody>
+      </table>
+      </div>
+    );
+    }
+}
---
+++ client/src/index.js
@@ -0,0 +1,265 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+import FoodSelector from './components/FoodSelector'
+
+let sampleProps = {
+  "searchedFood": {
+    "ndbno": "45174798",
+    "name": "A CHRISTMAS TRADITION MUG SET, UPC: 041376400094",
+    "ds": "LI",
+    "manu": "THE ELF ON THE SHELF",
+    "ru": "g",
+    "ing": {
+      "desc": "COCOA INGREDIENTS: SUGAR, CORN SYRUP SOLIDS, DAIRY PRODUCT SOLIDS (FROM MILK), VEGETABLE OIL (PARTIALLY HYDROGENATED COCONUT OR PALM KERNEL AND HYDROGENATED SOYBEAN), MARSHMALLOWS (SUGAR, CORN SYRUP, DEXTROSE, MODIFIED CORN STARCH, GELATIN, ARTIFICIAL AND NATURAL FLAVOR), COCOA PROCESSED WITH ALKALI, AND LESS THAN 2% OF SALT, CELLULOSE GUM, SODIUM CASEINATE, DIPOTASSIUM PHOSPHATE, SODIUM ALUMINOSILICATE, MONO- AND DIGLYCERIDES, GUAR GUM, ARTIFICIAL FLAVOR, SUCRALOSE., MARSHMALLOW INGREDIENTS: CORN SYRUP, SUGAR, SORBITOL, GELATIN, MODIFIED CORN STARCH, ARTIFICIAL FLAVOR, CITRIC ACID.",
+      "upd": "07/14/2017"
+    },
+    "nutrients": [
+      {
+        "nutrient_id": "208",
+        "name": "Energy",
+        "derivation": "LCCS",
+        "group": "Proximates",
+        "unit": "kcal",
+        "value": "400",
+        "measures": [
+          {
+            "label": "PACKAGE",
+            "eqv": 20,
+            "eunit": "g",
+            "qty": 1,
+            "value": "80"
+          }
+        ]
+      },
+      {
+        "nutrient_id": "203",
+        "name": "Protein",
+        "derivation": "LCSL",
+        "group": "Proximates",
+        "unit": "g",
+        "value": "5.00",
+        "measures": [
+          {
+            "label": "PACKAGE",
+            "eqv": 20,
+            "eunit": "g",
+            "qty": 1,
+            "value": "1.00"
+          }
+        ]
+      },
+      {
+        "nutrient_id": "204",
+        "name": "Total lipid (fat)",
+        "derivation": "LCCS",
+        "group": "Proximates",
+        "unit": "g",
+        "value": "10.00",
+        "measures": [
+          {
+            "label": "PACKAGE",
+            "eqv": 20,
+            "eunit": "g",
+            "qty": 1,
+            "value": "2.00"
+          }
+        ]
+      },
+      {
+        "nutrient_id": "205",
+        "name": "Carbohydrate, by difference",
+        "derivation": "LCCS",
+        "group": "Proximates",
+        "unit": "g",
+        "value": "80.00",
+        "measures": [
+          {
+            "label": "PACKAGE",
+            "eqv": 20,
+            "eunit": "g",
+            "qty": 1,
+            "value": "16.00"
+          }
+        ]
+      },
+      {
+        "nutrient_id": "291",
+        "name": "Fiber, total dietary",
+        "derivation": "LCSL",
+        "group": "Proximates",
+        "unit": "g",
+        "value": "5.0",
+        "measures": [
+          {
+            "label": "PACKAGE",
+            "eqv": 20,
+            "eunit": "g",
+            "qty": 1,
+            "value": "1.0"
+          }
+        ]
+      },
+      {
+        "nutrient_id": "269",
+        "name": "Sugars, total",
+        "derivation": "LCCS",
+        "group": "Proximates",
+        "unit": "g",
+        "value": "60.00",
+        "measures": [
+          {
+            "label": "PACKAGE",
+            "eqv": 20,
+            "eunit": "g",
+            "qty": 1,
+            "value": "12.00"
+          }
+        ]
+      },
+      {
+        "nutrient_id": "301",
+        "name": "Calcium, Ca",
+        "derivation": "LCCD",
+        "group": "Minerals",
+        "unit": "mg",
+        "value": "0",
+        "measures": [
+          {
+            "label": "PACKAGE",
+            "eqv": 20,
+            "eunit": "g",
+            "qty": 1,
+            "value": "0"
+          }
+        ]
+      },
+      {
+        "nutrient_id": "303",
+        "name": "Iron, Fe",
+        "derivation": "LCCD",
+        "group": "Minerals",
+        "unit": "mg",
+        "value": "0.00",
+        "measures": [
+          {
+            "label": "PACKAGE",
+            "eqv": 20,
+            "eunit": "g",
+            "qty": 1,
+            "value": "0.00"
+          }
+        ]
+      },
+      {
+        "nutrient_id": "307",
+        "name": "Sodium, Na",
+        "derivation": "LCCS",
+        "group": "Minerals",
+        "unit": "mg",
+        "value": "850",
+        "measures": [
+          {
+            "label": "PACKAGE",
+            "eqv": 20,
+            "eunit": "g",
+            "qty": 1,
+            "value": "170"
+          }
+        ]
+      },
+      {
+        "nutrient_id": "401",
+        "name": "Vitamin C, total ascorbic acid",
+        "derivation": "LCCD",
+        "group": "Vitamins",
+        "unit": "mg",
+        "value": "0.0",
+        "measures": [
+          {
+            "label": "PACKAGE",
+            "eqv": 20,
+            "eunit": "g",
+            "qty": 1,
+            "value": "0.0"
+          }
+        ]
+      },
+      {
+        "nutrient_id": "318",
+        "name": "Vitamin A, IU",
+        "derivation": "LCCD",
+        "group": "Vitamins",
+        "unit": "IU",
+        "value": "0",
+        "measures": [
+          {
+            "label": "PACKAGE",
+            "eqv": 20,
+            "eunit": "g",
+            "qty": 1,
+            "value": "0"
+          }
+        ]
+      },
+      {
+        "nutrient_id": "606",
+        "name": "Fatty acids, total saturated",
+        "derivation": "LCCS",
+        "group": "Lipids",
+        "unit": "g",
+        "value": "10.000",
+        "measures": [
+          {
+            "label": "PACKAGE",
+            "eqv": 20,
+            "eunit": "g",
+            "qty": 1,
+            "value": "2.000"
+          }
+        ]
+      },
+      {
+        "nutrient_id": "605",
+        "name": "Fatty acids, total trans",
+        "derivation": "LCCS",
+        "group": "Lipids",
+        "unit": "g",
+        "value": "0.000",
+        "measures": [
+          {
+            "label": "PACKAGE",
+            "eqv": 20,
+            "eunit": "g",
+            "qty": 1,
+            "value": "0.000"
+          }
+        ]
+      },
+      {
+        "nutrient_id": "601",
+        "name": "Cholesterol",
+        "derivation": "LCCS",
+        "group": "Lipids",
+        "unit": "mg",
+        "value": "0",
+        "measures": [
+          {
+            "label": "PACKAGE",
+            "eqv": 20,
+            "eunit": "g",
+            "qty": 1,
+            "value": "0"
+          }
+        ]
+      }
+    ]
+  },
+  "searchOrDaily": true,
+  "day": "March 15, 2018"
+}
+
+ReactDOM.render(<FoodSelector />, document.getElementById('root'));
+registerServiceWorker();
