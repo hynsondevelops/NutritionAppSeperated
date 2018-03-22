54450a1646d4912570f265584bf5cffa64ca134f
Adam Hynson <hynsondevelops@gmail.com>
2018-03-21 20:53:53 -0400

    Added food portion and daily diet

--- NutritionAPI/db/schema.rb
+++ NutritionAPI/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180321184242) do
+ActiveRecord::Schema.define(version: 20180322005303) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string   "namespace"
@@ -41,6 +41,21 @@ ActiveRecord::Schema.define(version: 20180321184242) do
     t.datetime "updated_at",                          null: false
     t.index ["email"], name: "index_admin_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true
+  end
+
+  create_table "daily_diets", force: :cascade do |t|
+    t.datetime "date"
+    t.integer  "admin_user_id"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+  end
+
+  create_table "food_portions", force: :cascade do |t|
+    t.decimal  "quantity"
+    t.integer  "food_id"
+    t.integer  "daily_diet_id"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
   end
 
   create_table "foods", force: :cascade do |t|
